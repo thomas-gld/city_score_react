d1454a1eb5a927b9eccfce5969d812bb8415bc3c
thomas_g <thomas.gladin.pro@gmail.com>
2026-02-24 16:37:20 +0100

    Hide db credentials in .env file

--- frontend/src/components/FormCategories.jsx
+++ frontend/src/components/FormCategories.jsx
@@ -7,7 +7,7 @@ export default function FormCategories() {
     const [categoriesChoice, setCategoriesChoice] = useState([])
 
     const categoriesSelect = categories.map(c => {
-        return <button key={c} onClick={() => categoriesChoice.includes(c) ? setCategoriesChoice(categoriesChoice.filter(el => el !== c)) : setCategoriesChoice([...categoriesChoice, c])} className={`hover:cursor-pointer text-xl font-serif font-bold border-2 m-5 p-3 rounded-xl transition-all duration-200 ${categoriesChoice.includes(c) ? "bg-yellow-400 border-yellow-600 text-purple-900 scale-105 shadow-lg" : "bg-yellow-100 border-yellow-300 text-black hover:bg-yellow-200"}`}>{c}</button>
+        return (<button key={c} onClick={() => categoriesChoice.includes(c) ? setCategoriesChoice(categoriesChoice.filter(el => el !== c)) : setCategoriesChoice([...categoriesChoice, c])} className={`hover:cursor-pointer text-xl font-serif font-bold border-2 m-5 p-3 rounded-xl transition-all duration-200 ${categoriesChoice.includes(c) ? "bg-yellow-400 border-yellow-600 text-purple-900 scale-105 shadow-lg" : "bg-yellow-100 border-yellow-300 text-black hover:bg-yellow-200"}`}>{c}</button>)
     })
 
     function handleNext() {
