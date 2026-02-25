3b08696a1d54dae0928de3fdfbe05ab2288e13aa
thomas_g <thomas.gladin.pro@gmail.com>
2026-02-25 14:45:09 +0100

    Change python names in snake_case

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Users
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .utils.rank_city import rank_city
 
 
 
@@ -10,3 +11,8 @@ from rest_framework.decorators import api_view
 @api_view(["GET"])
 def test(request):
     return Response({"status": "ok"})
+
+
+@api_view(["GET"])
+def city_score(request):
+    pass
