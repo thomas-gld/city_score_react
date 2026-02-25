fa12b8a4b1e4c658061412283d96ac2305d25bec
thomas_g <thomas.gladin.pro@gmail.com>
2026-02-25 15:14:13 +0100

    Make POST rank_city in views.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import test
+from .views import test, city_score
 
 urlpatterns = [
     path('test/', test),
+    path('cityscore/', city_score)
 
 
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from .models import Users
+from .models import Users, Ville
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .utils.rank_city import rank_city
 
 
@@ -13,6 +13,22 @@ def test(request):
     return Response({"status": "ok"})
 
 
-@api_view(["GET"])
+@api_view(["POST"])
 def city_score(request):
-    pass
+    user_pref = request.data
+
+    required_keys = ["criteres", "lieux", "meteo", "categories", "important"]
+    for key in required_keys:
+        if key not in user_pref:
+            return Response(
+                {"error": f"Champ manquant : {key}"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    city_list = list(Ville.objects.values_list("name", flat=True))
+
+    scores = rank_city(city_list, user_pref)
+
+    result = {city: round(score, 2) for city, score in sorted(scores.items(), key=lambda x: x[1], reverse=True)}
+
+    return Response(result, status=status.HTTP_200_OK)
