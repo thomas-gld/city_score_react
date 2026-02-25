35fa02a50e1485401bf7a333eddac78b3108c9b9
thomas_g <thomas.gladin.pro@gmail.com>
2026-02-25 11:41:49 +0100

    Add Home.jsx

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+export default function Home() {
+
+
+    return (
+        <>
+        <div>
+            <h1>
+                Quelle ville te correspond le plus ?
+            </h1>
+        </div>
+        <div>
+            <link href="/criteres" />
+        </div>
+        </>
+    )
+}
