1aa0de551e9e93d29afd0792b62dc3a6a7dc25bf
thomas_g <thomas.gladin.pro@gmail.com>
2026-02-25 11:56:30 +0100

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,17 +1,25 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
+    const navigate = useNavigate()
 
+    function handleStart() {
+        navigate('/criteres')
+    }
 
     return (
         <>
-        <div>
+        <div className="min-h-screen bg-linear-to-tr from-purple-200 to-purple-800 flex flex-col items-center font-serif leading-relaxed font-bold text-purple-900">
+        <div className="bg-yellow-200 text-4xl p-5 mt-10 rounded-lg shadow-lg/30 font-serif">
             <h1>
                 Quelle ville te correspond le plus ?
             </h1>
         </div>
-        <div>
-            <link href="/criteres" />
+        <div className="text-2xl p-3 bg-amber-100 mt-10 rounded-lg shadow-xl/10 hover:cursor-pointer hover:bg-yellow-300">
+            <button onClick={handleStart} className="hover:cursor-pointer hover:bg-yellow-300">
+                Commencer    
+            </button>   
+        </div>
         </div>
         </>
     )
