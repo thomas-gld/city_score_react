60acd55029f41072fbd50d13f11c9d52fdc6d708
thomas_g <thomas.gladin.pro@gmail.com>
2026-02-25 11:11:28 +0100

    Update App.jsx with Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes, Navigate } from "react-router-dom"
 import FormCriteres from "./components/FormCriteres"
 import FormLieux from "./components/FormLieux"
 import FormCategories from "./components/FormCategories"
@@ -6,14 +7,16 @@ import FormLoisirs from "./components/FormLoisir";
 import FormMeteo  from "./components/FormMeteo";
 
 function App() {
-  return <> 
-    <FormCategories/>
-    <FormEstImportant/>
-    <FormLieux/>
-    <FormCriteres/>
-    <FormLoisirs/>
-    <FormMeteo/>
-    </>
+  return (
+    <Routes>
+      <Route path="/categories" element={<FormCategories/>}/>
+      <Route path="/criteres" element={<FormCriteres/>}/>
+      <Route path="/lieux" element={<FormLieux/>}/>
+      <Route path="/est-important" element={<FormEstImportant/>}/>
+      <Route path="/loisirs" element={<FormLoisirs/>}/>
+      <Route path="/meteo" element={<FormMeteo/>}/>
+    </Routes>
+  )
 }
 
 export default App
