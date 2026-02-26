613b0e00f385749d2e4b72db9393f756cd0357ed
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-02-26 15:33:43 +0100

    Fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+  server: {
+      port: 8080,
+      open: true,
+      host: true, // allows for external device connection on local network
+      proxy: {
+         // prevent CORS error in dev when backend and frontend servers run on different ports
+         '^/api/.*': {
+            target: 'http://localhost:8000',
+            changeOrigin: true,
+         },
+      }
+   }
 })
